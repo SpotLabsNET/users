b8797311d26516a7dea385fc3b18135f356287af
Jevon Wright <jevon@jevon.org>
2015-04-27 10:29:22 +0000

    Fix Google OpenID identity mapping
    It turns out that the returned web token is not actually complete base64, but "safe" base64 encoded.
    
    Issue #266

--- src/GoogleWithOpenID.php
+++ src/GoogleWithOpenID.php
@@ -13,19 +13,33 @@ class GoogleWithOpenID extends Google {
   var $id_token = false;
 
   public function getAuthorizationUrl($options = []) {
-    return parent::getAuthorizationUrl($options) . '&openid.realm=' . "http://" . \Openclerk\Config::get('openid_host');
+    return parent::getAuthorizationUrl($options) . '&openid.realm=' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']) ? "https://" : "http://") . \Openclerk\Config::get('openid_host');
+  }
+
+  /**
+   * Decode a string with URL-safe Base64.
+   *
+   * @param string $input A Base64 encoded string
+   *
+   * @return string A decoded string
+   */
+  public static function safe_base64_decode($input) {
+    $remainder = strlen($input) % 4;
+    if ($remainder) {
+      $padlen = 4 - $remainder;
+      $input .= str_repeat('=', $padlen);
+    }
+    return base64_decode(strtr($input, '-_', '+/'));
   }
 
   protected function prepareAccessTokenResult(array $result) {
     if (isset($result['id_token'])) {
-      $id_token = $result['id_token'];
-
       // [signature, token, ???]
       $id_token_bits = explode(".", $result['id_token']);
 
       // we could validate the token here but eh
       if (count($id_token_bits) >= 2) {
-        $this->id_token = json_decode(base64_decode($id_token_bits[1]), true /* assoc */);
+        $this->id_token = json_decode(self::safe_base64_decode($id_token_bits[1]), true /* assoc */);
       }
     }
 
